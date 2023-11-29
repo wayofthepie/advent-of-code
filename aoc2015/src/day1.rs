89b2907890df97c9e17e11b37d874f448110f89e
wayofthepie <wayofthepie@gmail.com>
2023-11-29 21:32:40 +0000

    Add 2015 day1 part2

--- aoc2015/src/day1.rs
+++ aoc2015/src/day1.rs
@@ -6,9 +6,30 @@ pub fn part_one(input: &str) -> isize {
     })
 }
 
+pub fn part_two(input: &str) -> isize {
+    let mut position = 1;
+    let mut floor = 0;
+    for brace in input.chars() {
+        floor = move_floor(floor, brace);
+        if floor == -1 {
+            break;
+        }
+        position += 1;
+    }
+    position
+}
+
+fn move_floor(floor: isize, brace: char) -> isize {
+    match brace {
+        '(' => floor + 1,
+        ')' => floor - 1,
+        _ => floor,
+    }
+}
+
 #[cfg(test)]
 mod test {
-    use super::part_one;
+    use super::{part_one, part_two};
 
     #[test]
     fn example1() {
@@ -32,9 +53,30 @@ mod test {
     }
 
     #[test]
-    fn test_part_one() {
+    fn test_part1() {
         let input = include_str!("../resources/day1_part1");
         let result = part_one(input);
         assert_eq!(result, 280);
+    }
+
+    #[test]
+    fn example1_part2() {
+        let input = ")";
+        let result = part_two(input);
+        assert_eq!(result, 1);
+    }
+
+    #[test]
+    fn example2_part2() {
+        let input = "()())";
+        let result = part_two(input);
+        assert_eq!(result, 5);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = include_str!("../resources/day1_part1");
+        let result = part_two(input);
+        assert_eq!(result, 1797);
     }
 }
