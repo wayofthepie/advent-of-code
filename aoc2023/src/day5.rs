d25e44d4746e482f9128171531d8457cda372f78
wayofthepie <wayofthepie@gmail.com>
2023-12-05 09:03:34 +0000

    2023: Add day5, with brute force solution for part2

---
+++ aoc2023/src/day5.rs
@@ -0,0 +1,236 @@
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{digit1, newline},
+    combinator::eof,
+    multi::{many_till, separated_list1},
+    sequence::tuple,
+    IResult,
+};
+use rayon::prelude::*;
+
+use crate::ws;
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+struct RangeMap {
+    src: isize,
+    dest: isize,
+    len: isize,
+}
+
+type MappingRanges = Vec<Vec<Vec<usize>>>;
+
+impl RangeMap {
+    fn new(src: isize, dest: isize, len: isize) -> Self {
+        Self { src, dest, len }
+    }
+}
+
+pub fn part_one(input: &str) -> usize {
+    let (seeds, steps) = seeds_and_steps(input);
+    seeds.into_iter().fold(usize::MAX, |acc, seed| {
+        let n = steps.iter().fold(seed as isize, |acc, step| {
+            step.iter()
+                .find(|range| acc >= range.src && acc < range.src + range.len)
+                .map(|range| {
+                    let diff = range.dest - range.src;
+                    acc + diff
+                })
+                .unwrap_or(acc)
+        });
+        acc.min(n as usize)
+    })
+}
+
+pub fn part_two(input: &str) -> usize {
+    let (seeds, steps) = seeds_and_steps(input);
+    seeds
+        .chunks(2)
+        .map(|chunk| (chunk[0], chunk[1]))
+        .par_bridge()
+        .flat_map(|(start, len)| (start..start + len))
+        .fold(|| usize::MAX, fold_seeds(&steps))
+        .min()
+        .unwrap_or(0)
+}
+
+fn fold_seeds(steps: &[Vec<RangeMap>]) -> impl Fn(usize, usize) -> usize + '_ {
+    |acc, seed| {
+        let n = steps.iter().fold(seed as isize, |acc, step| {
+            step.iter()
+                .find(|range| acc >= range.src && acc < range.src + range.len)
+                .map(|range| {
+                    let diff = range.dest - range.src;
+                    acc + diff
+                })
+                .unwrap_or(acc)
+        });
+        acc.min(n as usize)
+    }
+}
+
+fn seeds_and_steps(input: &str) -> (Vec<usize>, Vec<Vec<RangeMap>>) {
+    let (_, (seeds, mappings)) = parse(input).unwrap();
+    let steps = mappings
+        .into_iter()
+        .map(|values| {
+            values
+                .into_iter()
+                .map(|range| {
+                    let &[dest, src, len] = range.as_slice() else {
+                        panic!();
+                    };
+                    RangeMap::new(src as isize, dest as isize, len as isize)
+                })
+                .collect()
+        })
+        .collect::<Vec<Vec<RangeMap>>>();
+    (seeds, steps)
+}
+
+fn parse(input: &str) -> IResult<&str, (Vec<usize>, MappingRanges)> {
+    let (rest, seeds) = parse_seeds(input)?;
+    let (rest, (mappings, _)) = many_till(ws(parse_ranges), eof)(rest)?;
+    let mappings = mappings
+        .into_iter()
+        .reduce(|mut acc, m| {
+            acc.extend(m);
+            acc
+        })
+        .unwrap();
+    Ok((rest, (seeds, mappings)))
+}
+
+fn parse_seeds(input: &str) -> IResult<&str, Vec<usize>> {
+    let (rest, _) = tag("seeds: ")(input)?;
+    let (rest, nums) = separated_list1(tag(" "), digit1)(rest)?;
+    let nums = nums
+        .into_iter()
+        .map(|num| num.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>();
+    Ok((rest, nums))
+}
+
+fn parse_ranges(input: &str) -> IResult<&str, MappingRanges> {
+    let parse_id = alt((
+        tag("seed-to-soil"),
+        tag("soil-to-fertilizer"),
+        tag("fertilizer-to-water"),
+        tag("water-to-light"),
+        tag("light-to-temperature"),
+        tag("temperature-to-humidity"),
+        tag("humidity-to-location"),
+    ));
+    let (rest, (_, _, ranges)) = tuple((
+        parse_id,
+        ws(tag("map:")),
+        separated_list1(newline, separated_list1(tag(" "), digit1)),
+    ))(input)?;
+    let ranges = ranges
+        .into_iter()
+        .map(|entry| {
+            entry
+                .into_iter()
+                .map(|num| num.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>()
+        })
+        .collect::<Vec<Vec<usize>>>();
+    Ok((rest, vec![ranges]))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_one_example1() {
+        let input = r#"seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4"#;
+        let result = part_one(input);
+        assert_eq!(result, 35);
+    }
+
+    #[test]
+    fn part_one_test() {
+        let input = include_str!("../resources/day5_part1");
+        let result = part_one(input);
+        assert_eq!(result, 836040384);
+    }
+
+    #[test]
+    fn part_two_example1() {
+        let input = r#"seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4"#;
+        let result = part_two(input);
+        assert_eq!(result, 46);
+    }
+
+    #[test]
+    fn part_two_test() {
+        let input = include_str!("../resources/day5_part1");
+        let result = part_two(input);
+        assert_eq!(result, 10834440);
+    }
+}
--- aoc2023/src/lib.rs
+++ aoc2023/src/lib.rs
@@ -1,4 +1,16 @@
+use nom::{character::complete::multispace0, error::ParseError, sequence::delimited, IResult};
+
 pub mod day1;
 pub mod day2;
 pub mod day3;
 pub mod day4;
+pub mod day5;
+
+fn ws<'a, F: 'a, O, E: ParseError<&'a str>>(
+    inner: F,
+) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+where
+    F: Fn(&'a str) -> IResult<&'a str, O, E>,
+{
+    delimited(multispace0, inner, multispace0)
+}
